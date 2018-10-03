589ba515f53d075e120633ebe4d998d5559791ab
willisbillis <elliott.williams14@gmail.com>
2018-10-03 16:53:27 -0400

    Parallel processing structure and consoladation
    Changed parallel processing method to use multiprocessing module instead of MPI. Generalized to be used for xy and xz views.

--- vel_disp.py
+++ vel_disp.py
@@ -1,13 +1,13 @@
 # Written by Elliott Williams https://github.com/willisbillis/yt
 
-import yt,time,multiprocessing,matplotlib,sys
-import numpy as np
-#from multiprocessing import Pool, Process, Array
-from math import pi,sqrt,e
-from scipy.integrate import quad
+import yt,time,matplotlib,sys,os,glob,random
 import numpy as np
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import multiprocessing as mp
+from math import pi,sqrt,e
+from scipy.integrate import quad
 from lmfit.models import GaussianModel
 
 def timer(t):
@@ -17,25 +17,18 @@ def timer(t):
     elif t <= 7200:
         t = float(t)/60
         return "Elapsed time: %.1f min" % t
-
-# NOTE you can definitely parallelize this for higher resolution if you split up
-# the task of integrating and calculating the velocity dispersion for each (x,z)
-# coordinate across multiple instances/threads/processors
-#
-# Benchmarks (single processor of Intel Q9450 @ 2.66GHz):
-# Level 1 - 7 sec
-# Level 2 - 43 sec
-# Level 3 - 5.6 min
 ################################################################################
 # set resolution level of png (dpi)
 quick = 150
 pro = 1250
-dpi_level = pro
+dpi_level = quick
 # Turn on/off FWHM (assumes vel disp along LOS is Gaussian, which is valid)
 FWHM = False
 # Ionization cutoff temp for H
 HI_min = 0
 HI_max = 10000
+# Assumed distance to the MS - used to calculate angular resolution
+MS_dist = 65 # kpc
 # Parameters for velocity dispersion calculation
 num_bins = 200    # affects line resolution. more bins >> higher resolution
 vel_min = -100    # km/s
@@ -45,25 +38,35 @@ bins = np.linspace(vel_min,vel_max,num=num_bins,retstep=True)
 def gaussian_curve(x,mu,sigma,A):
     return (A/(sigma*sqrt(2*pi)))*e**(-0.5*((x-mu)/sigma)**2)
 
-def fixed_res_array(ds,level):
+def fixed_res_array(file_name,level,order):
+    # normal == x,z,y (0,2,1)
+    # xy == y,x,z (1,0,2)
+    t1 = order.index('x')
+    t2 = order.index('y')
+    t3 = order.index('z')
+    trans_matrix = [t1,t2,t3]
+
+    LOS_velocity = "velocity_" + order[2]
+    ds = yt.load(file_name)
     all_data_level_x = ds.covering_grid(level=level,left_edge=ds.domain_left_edge,dims=ds.domain_dimensions*2**level)
     disp_array = []
     ds.periodicity = (True,True,True)
     k = 1.380649e-23 # J/K
     m_avg = 1.6737236e-27 # kg (mass of hydrogen atom)
-    for x in xrange(0,ds.domain_dimensions[0]*2**level):
+    for q1 in xrange(all_data_level_x.ActiveDimensions[t1]):
         vbin = []
-        for z in xrange(0,ds.domain_dimensions[2]*2**level):
+        for q2 in xrange(all_data_level_x.ActiveDimensions[t2]):
             weights = np.array([])
             weights = np.resize(weights,(num_bins-1,1))
-            for y in xrange(0,ds.domain_dimensions[1]*2**level):
-                temp = all_data_level_x["temperature"][x,y,z]
+            for q3 in xrange(all_data_level_x.ActiveDimensions[t3]):
+                coords = tuple([q1,q2,q3][i] for i in trans_matrix)
+                temp = float(all_data_level_x["temperature"][coords])
                 if temp <= HI_max and temp >= HI_min:
                     # velocity dispersion calculation
-                    weights = np.array([])
-                    weights = np.resize(weights,(num_bins-1,1))
-                    vel = float(all_data_level_x["velocity_y"][x,y,z].in_units("km/s"))
-                    mass = float(m_avg*all_data_level_x["H_nuclei_density"][x,y,z]*all_data_level_x["cell_volume"][x,y,z])
+                    cell_weights = np.array([])
+                    cell_weights = np.resize(weights,(num_bins-1,1))
+                    vel = float(all_data_level_x[LOS_velocity][coords].in_units("km/s"))
+                    mass = float(m_avg*all_data_level_x["H_nuclei_density"][coords]*all_data_level_x["cell_volume"][coords])
                     tb = float(sqrt(2*k*temp/m_avg)/1000)
 
                     mu = vel
@@ -72,11 +75,16 @@ def fixed_res_array(ds,level):
 
                     for n in xrange(0,len(bins[0])-1):
                         I = quad(gaussian_curve, bins[0][n], bins[0][n+1],args=(mu,sigma,A))
+                        cell_weights[n] += I[0]
                         weights[n] += I[0]
                     if (A-sum(weights))/A > 0.05:
-                        print " Values out of range. Expand velocity limits."
+                        if vel < (vel_max+vel_min)*0.5:
+                            print " Values out of range. Lower velocity limits."
+                            print " Out of range vel: " + str(vel) + " km/s"
+                        if vel > (vel_max+vel_min)*0.5:
+                            print " Values out of range. Raise velocity limits."
+                            print " Out of range vel: " + str(vel) + " km/s"
                         sys.exit()
-                    #fig = plt.hist(bins[0][:-1],bins=bins[0],weights=weights.flatten())
 
             if sum(weights) == 0: # all cells on sightline have T outside HI_min < T < HI_max
                 sigma = 0
@@ -91,46 +99,48 @@ def fixed_res_array(ds,level):
                     diff_sum += b*(a-avg_vel)**2
                 sigma = sqrt(diff_sum/sum(weights))
 
-                #plt.xlabel("Velocity")
-                #plt.ylabel("Frequency")
-                #plt.savefig("hist_{0}_{1}.png".format(x,z))
-                #plt.cla()
             if FWHM == True:
                 vbin.append(2*sqrt(2*ln(2))*sigma)
             elif FWHM == False:
                 vbin.append(sigma)
-            disp_array.append(vbin)
-        print "{0:.1f} %".format((x+1)*100/float(16*2**level))
+        disp_array.append(vbin)
+        prog = (q1+1)*100/float(all_data_level_x.ActiveDimensions[t1])
+        if prog % 10 < 100/float(all_data_level_x.ActiveDimensions[t1]):
+            print "{0:.1f} %".format(prog)
 
     da = np.array(disp_array)
     print "fixed resolution array created"
-    left = ds.domain_left_edge.in_units("kpc")[2]
-    right = ds.domain_right_edge.in_units("kpc")[2]
-    top = ds.domain_left_edge.in_units("kpc")[0]
-    bottom = ds.domain_right_edge.in_units("kpc")[0]
+    left = ds.domain_left_edge.in_units("kpc")[t2]
+    right = ds.domain_right_edge.in_units("kpc")[t2]
+    top = ds.domain_left_edge.in_units("kpc")[t1]
+    bottom = ds.domain_right_edge.in_units("kpc")[t1]
     domain = [left,right,top,bottom]
-    MS_dist = 65 # kpc
-    beam_width = (all_data_level_x["dx"][0,0,0]/(MS_dist*3.08568E18))*3600
+    dx = all_data_level_x["dx"]
+    return da,domain,dx
+
+def ang_resolution(dist,dx):
+    beam_width = (dx[0,0,0]/(dist*3.08568E18))*3600
     if beam_width >= 3600:
         beam_width = beam_width/3600
-        print "At MS distance of %d kpc, beam width is %.1f deg." % (MS_dist,beam_width)
+        print "At MS distance of %d kpc, beam width is %.1f deg." % (dist,beam_width)
     elif beam_width >= 60:
         beam_width = beam_width/60
-        print "At MS distance of %d kpc, beam width is %.1f arcmin." % (MS_dist,beam_width)
+        print "At MS distance of %d kpc, beam width is %.1f arcmin." % (dist,beam_width)
     else:
-        print "At MS distance of %d kpc, beam width is %.1f arcsec." % (MS_dist,beam_width)
+        print "At MS distance of %d kpc, beam width is %.1f arcsec." % (dist,beam_width)
     print "Velocity resolution: {0:.1f} km/s".format(bins[1])
-    return da,domain
 
-def main(data_array,domain_array,fl_nm,level):
-    import matplotlib as mpl
-    mpl.use('Agg')
-    import matplotlib.pyplot as plt
-    mpl.rcParams['font.size'] = 10
-    im = plt.imshow(data_array,origin = "lower",aspect = "equal",extent=domain_array,cmap="jet",interpolation='none')
-    plt.xlabel("z (kpc)")
-    plt.ylabel("x (kpc)")
-    cbar = plt.colorbar(pad=0,shrink=0.2275,aspect=6)
+def plotdata(data_array,domain_array,fl_nm,level,order):
+    matplotlib.rcParams['font.size'] = 10
+    fn = random.randint(0,500)
+    fig = plt.figure(fn)
+    ax = plt.gca()
+    im = ax.imshow(data_array,origin = "lower",aspect = "equal",extent=domain_array,cmap="jet",interpolation='none')
+    plt.xlabel(order[1] + " (kpc)")
+    plt.ylabel(order[0] + " (kpc)")
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = plt.colorbar(im, cax=cax)
     cbar.ax.tick_params(labelsize=8)
     cbar.set_label("Velocity Dispersion (km/s)",size=7)
 
@@ -141,18 +151,46 @@ def main(data_array,domain_array,fl_nm,level):
     print "File saved as: " + fig_nm
     return
 
-"""
-if __name__ == "__main__":
-    pool = multiprocessing.Pool(4)
-    pool.map(main,da)
-"""
+def save_data(fl_list,level,order):
+    # saves sliceplots to separate directory inside working directory
+    directory = os.getcwd() + "/vel_disp_lvl_{0}".format(level)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for fl_nm in fl_list:
+        source_file = os.getcwd() + "/velocity_{0}disp_{1}_lvl_{2}.png".format(''.join(order[:1]),fl_nm[-4:],level)
+        destination = os.getcwd() + "/vel_{0}disp_lvl_{2}/velocity_{0}disp_{1}_lvl_{2}.png".format(''.join(order[:1]),fl_nm[-4:],level)
+        if os.path.exists(source_file):
+            print "moving {}...".format(source_file)
+            os.rename(source_file, destination)
+    print "Data saved to: {1}/vel_{0}disp_lvl_{2}".format(''.join(order[:1]),os.getcwd(),level)
+
+def main((fl_nm,level,xyz_order)):
+    da,domain,dx = fixed_res_array(fl_nm,level,xyz_order)
+    ang_resolution(MS_dist,dx)
+    plotdata(da,domain,fl_nm,level,xyz_order)
+    return
 
 if __name__ == "__main__":
-    fl_nm = raw_input("enter filename: ").strip()
+    full = raw_input("run all files? (y/N): ")
+    fl_nm = os.getcwd() +  "/" + raw_input("enter filename: ").strip()
     level = int(raw_input("resolution level: ").strip())
+    view = raw_input("XY view? (y/N): ")
+    if view == "y":
+        xyz_order = ['x','y','z']
+    else:
+        xyz_order = ['x','z','y']
+    if full == "y":
+        file_list = glob.glob(fl_nm[0:-4] + "*")
+    else:
+        file_list = [fl_nm]
+
+    arg_list = zip(file_list,[level]*len(file_list),[xyz_order]*len(file_list))
     start_time = time.time()
-    ds = yt.load(fl_nm)
-    da,domain = fixed_res_array(ds,level)
-    main(da,domain,fl_nm,level)
+    cpu_cores = mp.cpu_count()
+    pool = mp.Pool(processes=(cpu_cores-1))
+    result = pool.map(main,arg_list)
+    pool.close()
+    pool.join()
+    save_data(file_list,level,xyz_order)
     stop_time = time.time()
     print timer(stop_time-start_time)
